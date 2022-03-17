65b52b3f6ba50cee7b5bcfb4a04bb70f2f6a977e
Joran Floor <joranfloor@gmail.com>
2022-03-17 10:16:25 +0100

    f

--- public/login.php
+++ public/login.php
@@ -35,88 +35,88 @@
                     </svg>
                     <div class="text-7xl mt-16 font-bold tracking-wide">Happening now</div>
                     <div class="font-bold mt-14 text-3xl tracking-wide">Join Twitter today.</div>
-                        <div class="w-80 mt-10 flex flex-col gap-3">
-                            <a href="../index.php">
+                    <div class="w-80 mt-10 flex flex-col gap-3">
+                        <a href="../index.php">
                             <div class="cursor-pointer hover:bg-gray-100 transition-all py-1.5 border rounded-full">
                                 <div class="flex justify-between">
                                     <div class="flex justify-between items-center">
-                    <div class="w-80 mt-10 flex flex-col gap-3">
-                        <div class="cursor-pointer hover:bg-gray-100 transition-all py-0.5 border rounded-full">
-                            <div class="flex justify-between">
-                                <div class="flex justify-between items-center">
-                                    <div>
-                                        <img class="w-6 ml-3 rounded-full" src="https://pbs.twimg.com/profile_images/760478349198258178/ADaMUQSF_400x400.jpg">
-                                    </div>
-                                    <div class="ml-3">
-                                        <div class="text-xs font-medium">Sign in as Politie Basisteam Ede</div>
-                                        <div class="flex items-center -mt-1">
-                                            <div class="text-gray-600 text-xs">politieede@gmail.com</div>
-                                            <svg class="w-4 mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                                <path fill="#909090" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"></path>
-                                                <path fill="none" d="M0 0h24v24H0V0z"></path>
-                                            </svg>
+                                        <div class="w-80 mt-10 flex flex-col gap-3">
+                                            <div class="cursor-pointer hover:bg-gray-100 transition-all py-0.5 border rounded-full">
+                                                <div class="flex justify-between">
+                                                    <div class="flex justify-between items-center">
+                                                        <div>
+                                                            <img class="w-6 ml-3 rounded-full" src="https://pbs.twimg.com/profile_images/760478349198258178/ADaMUQSF_400x400.jpg">
+                                                        </div>
+                                                        <div class="ml-3">
+                                                            <div class="text-xs font-medium">Sign in as Politie Basisteam Ede</div>
+                                                            <div class="flex items-center -mt-1">
+                                                                <div class="text-gray-600 text-xs">politieede@gmail.com</div>
+                                                                <svg class="w-4 mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                                                    <path fill="#909090" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"></path>
+                                                                    <path fill="none" d="M0 0h24v24H0V0z"></path>
+                                                                </svg>
+                                                            </div>
+                                                        </div>
+                                                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png" class="ml-16 w-4">
+                                                    </div>
+                                                    <div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="cursor-pointer hover:bg-gray-100 transition-all py-2 border rounded-full">
+                                                <div class="flex text-center justify-center gap-2">
+                                                    <svg viewBox="0 0 24 24" aria-hidden="true" class="w-5" style="color: rgb(15, 20, 25);">
+                                                        <g>
+                                                            <path d="M16.365 1.43c0 1.14-.493 2.27-1.177 3.08-.744.9-1.99 1.57-2.987 1.57-.12 0-.23-.02-.3-.03-.01-.06-.04-.22-.04-.39 0-1.15.572-2.27 1.206-2.98.804-.94 2.142-1.64 3.248-1.68.03.13.05.28.05.43zm4.565 15.71c-.03.07-.463 1.58-1.518 3.12-.945 1.34-1.94 2.71-3.43 2.71-1.517 0-1.9-.88-3.63-.88-1.698 0-2.302.91-3.67.91-1.377 0-2.332-1.26-3.428-2.8-1.287-1.82-2.323-4.63-2.323-7.28 0-4.28 2.797-6.55 5.552-6.55 1.448 0 2.675.95 3.6.95.865 0 2.222-1.01 3.902-1.01.613 0 2.886.06 4.374 2.19-.13.09-2.383 1.37-2.383 4.19 0 3.26 2.854 4.42 2.955 4.45z"></path>
+                                                        </g>
+                                                    </svg>
+                                                    <div class="font-bold">Sign up with Apple</div>
+                                                </div>
+                                            </div>
+
+                                            <div class="flex items-center justify-between gap-2">
+                                                <div class="bg-gray-100 w-full" style="height: 1px;"></div>
+                                                <div class="-mt-1">or</div>
+                                                <div class="bg-gray-100 w-full" style="height: 1px;"></div>
+                                            </div>
+
+                                            <div>
+                                                <button class="bg-sky-500 font-bold w-full py-3 text-white rounded-full text-md transition-all hover:bg-sky-600">Sign up with phone or email</button>
+                                            </div>
+                                            <div>
+                                                <div class="text-gray-500" style="font-size: 0.68em;">By signing up, you agree to the <span class="text-sky-500 hover:underline cursor-pointer">Terms of Service</span> and <span class="text-sky-500 hover:underline cursor-pointer">Privacy Policy</span>, including <span class="text-sky-500 hover:underline cursor-pointer">Cookie Use.</span></div>
+                                            </div>
+                                            <div>
+                                                <div class="font-bold tracking-wide mt-10">Already have an account?</div>
+                                            </div>
+                                            <div>
+                                                <button class="font-semibold w-full py-2 text-sky-500 border rounded-full text-md transition-all hover:bg-sky-50">Sign in</button>
+                                            </div>
                                         </div>
                                     </div>
-                                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png" class="ml-16 w-4">
-                                </div>
-                                <div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="cursor-pointer hover:bg-gray-100 transition-all py-2 border rounded-full">
-                            <div class="flex text-center justify-center gap-2">
-                                <svg viewBox="0 0 24 24" aria-hidden="true" class="w-5" style="color: rgb(15, 20, 25);">
-                                    <g>
-                                        <path d="M16.365 1.43c0 1.14-.493 2.27-1.177 3.08-.744.9-1.99 1.57-2.987 1.57-.12 0-.23-.02-.3-.03-.01-.06-.04-.22-.04-.39 0-1.15.572-2.27 1.206-2.98.804-.94 2.142-1.64 3.248-1.68.03.13.05.28.05.43zm4.565 15.71c-.03.07-.463 1.58-1.518 3.12-.945 1.34-1.94 2.71-3.43 2.71-1.517 0-1.9-.88-3.63-.88-1.698 0-2.302.91-3.67.91-1.377 0-2.332-1.26-3.428-2.8-1.287-1.82-2.323-4.63-2.323-7.28 0-4.28 2.797-6.55 5.552-6.55 1.448 0 2.675.95 3.6.95.865 0 2.222-1.01 3.902-1.01.613 0 2.886.06 4.374 2.19-.13.09-2.383 1.37-2.383 4.19 0 3.26 2.854 4.42 2.955 4.45z"></path>
-                                    </g>
-                                </svg>
-                                <div class="font-bold">Sign up with Apple</div>
-                            </div>
-                        </div>
-
-                        <div class="flex items-center justify-between gap-2">
-                            <div class="bg-gray-100 w-full" style="height: 1px;"></div>
-                            <div class="-mt-1">or</div>
-                            <div class="bg-gray-100 w-full" style="height: 1px;"></div>
-                        </div>
-
-                        <div>
-                            <button class="bg-sky-500 font-bold w-full py-3 text-white rounded-full text-md transition-all hover:bg-sky-600">Sign up with phone or email</button>
-                        </div>
-                        <div>
-                            <div class="text-gray-500" style="font-size: 0.68em;">By signing up, you agree to the <span class="text-sky-500 hover:underline cursor-pointer">Terms of Service</span> and <span class="text-sky-500 hover:underline cursor-pointer">Privacy Policy</span>, including <span class="text-sky-500 hover:underline cursor-pointer">Cookie Use.</span></div>
-                        </div>
-                        <div>
-                            <div class="font-bold tracking-wide mt-10">Already have an account?</div>
-                        </div>
-                        <div>
-                            <button class="font-semibold w-full py-2 text-sky-500 border rounded-full text-md transition-all hover:bg-sky-50">Sign in</button>
-                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="w-full flex items-center justify-center h-12 bg-white bottom-0 fixed gap-4">
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">About</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Help center</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Terms of Service</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Privacy Policy</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Cookie Policy</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Accessibility</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Ads info</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Blog</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Status</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Careers</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Brand Resources</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Advertising</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Marketing</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Twitter for Bussiness</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Developers</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Directory</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Settings</a>
-        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">© 2022 Twitter, Inc.</a>
-    </div>
+                    <div class="w-full flex items-center justify-center h-12 bg-white bottom-0 fixed gap-4">
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">About</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Help center</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Terms of Service</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Privacy Policy</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Cookie Policy</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Accessibility</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Ads info</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Blog</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Status</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Careers</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Brand Resources</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Advertising</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Marketing</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Twitter for Bussiness</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Developers</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Directory</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">Settings</a>
+                        <a href="#" class="text-gray-600 cursor-pointer hover:underline" style="font-size: 0.8em">© 2022 Twitter, Inc.</a>
+                    </div>
 </body>
 
 </html>
